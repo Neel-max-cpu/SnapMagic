2286962a69d94ccc62ba0cecd90eddc97edd0998
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-05-26 13:35:31 +0530

    create request :memo:

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,30 @@
-
-
-// export default function Home() {
-//   return (
-//     <div className="bg-black pt-10 flex items-top justify-center h-svh">
-//       <main className="max-w-xl mx-auto">
-//         <h1 className="text-white text-3xl font-bold mb-8">
-//           Generate your AI Image
-//         </h1>
-
-//         <section className="max-w-full">
-//           <div className="flex items-center">
-//             <input
-//               type="text"
-//               id="prompt"
-//               name="prompt"
-//               className="rounded-l-lg py-3 px-4 w-full text-gray-300 focus:outline-none"
-//               placeholder="Enter your prompt here"
-//             />
-
-//             <button className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 rounded-r-lg py-3 px-4 ml-1 font-semibold">
-//               Generate
-//             </button>
-//           </div>
-//         </section>
-
-//         <section className="mt-8 max-w-full">
-//           <div className="flex items-center justify-center border-2 border-dashed border-gray-500 rounded-md w-full p-10">
-//             <div className="text-md text-gray-600">
-//               Image will be generated here!
-//             </div>
-//           </div>
-//         </section>
-//       </main>
-//     </div>
-//   );
-// }
-
-
 "use client";
 import { useState } from "react";
+
 
 export default function Home() {
   const [generatedImage, setGeneratedImage] = useState<string>("");
   const [prompt, setPrompt] = useState<string>("");
 
-  const generateImage = () => {
-    setGeneratedImage(prompt);
+  const generateImage = async () => { // <-- add "async" to this function
+    // setGeneratedImage(prompt);
+
+    if (prompt.length === 0) {
+      return;
+    }
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    });
+
+    const data = await response.json();
+    console.log(data);
   };
+
 
   return (
     
